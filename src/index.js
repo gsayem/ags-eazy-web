541369d556dcddca139e319a49f3070b481027d6
Sayem <sayemuiu@gmail.com>
2022-03-30 04:33:14 -0400

    fix dependency

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import Root from './containers/Root';
-import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import Root from "./containers/Root";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 const routes = (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/home" component={Root} />
-        <Redirect from="/" to="/home" />               
-      </Switch>
-    </BrowserRouter>
-  );
+  // <BrowserRouter>
+  //   <Switch>
+  //     <Route path="/home" component={Root} />
+  //     <Redirect from="/" to="/home" />
+  //   </Switch>
+  // </BrowserRouter>
+  <Routes>
+    <Route path="*" element={<Navigate to="/" replace={true} />} />
+    <Route path="/" element={<Root />}>
+      <Route index element={<Root />} />
+    </Route>
+  </Routes>
+);
 
-ReactDOM.render(routes,
-    document.getElementById('root'));
+ReactDOM.render(routes, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
